e8a198cccd691240a0f5ff793ccc131b0cd224d2
danielstena <32764564+danielstena@users.noreply.github.com>
2017-11-20 13:45:41 +0100

    uppdatering

--- script.js
+++ script.js
@@ -16,20 +16,30 @@ function attGoraLista(){
 
 $(document).ready(function(){
 
-    sessionStorage.ourUser = "test"
-    sessionStorage.ourPassword = "password"
     // Kör denna kod när sidan laddas
     var ourUser = "test"
     var ourPassword = "password"
+
+    //Detta är den nya koden ner till rad 55:
+    if (sessionStorage.ourUser == $(".userEmail").val())
+    {
+        $(".hejString").append(" Welcome " + $(".userEmail").val() );
+        $(".array").show();
+        $(".header1").hide();
+        $(".header2").show();
+        $(".forgott").hide();
+    } 
+        else {
 
     $(".welcome").hide();
     $(".forgott").hide();
     $(".header2").hide();
     // Klicka på logga in
     $(".loggaIn").click(function(){
-        
-        if (sessionStorage.ourUser == $(".userEmail").val() && sessionStorage.ourPassword == $(".userPassword").val()) {
+       
+        if (ourUser == $(".userEmail").val() && ourPassword == $(".userPassword").val()) {
                 // Dölj inlogg Visa hello
+                sessionStorage.ourUser;
                 $(".hejString").append(" Welcome " + $(".userEmail").val() );
                 $(".array").show();
                 $(".header1").hide();
@@ -42,6 +52,8 @@ $(document).ready(function(){
                 $(".signout").hide();
             }
     });
+}
+
     $(".signoutLink").click(function(){
         $(".header2").hide();
         $(".header1").show();
